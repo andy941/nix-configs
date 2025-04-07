c568aeb9b8171665ba52bdcf58e4708f47d68aa3
Andrea Finocchio <finocchio.andrea94@gmail.com>
2025-04-07 22:31:15 +0100

    Hyprlock improvement

--- hm_modules/hyprland/hyprlock.nix
+++ hm_modules/hyprland/hyprlock.nix
@@ -14,28 +14,6 @@
           brightness = 0.8;
           contrast = 0.8;
         };
-
-        input-field = {
-          size = "400, 100";
-          outline_thickness = 3;
-          dots_size = 0.2;
-          dots_spacing = 0.15;
-          dots_center = true;
-          fade_on_empty = false;
-          hide_input = false;
-          position = "0, -300";
-          halign = "center";
-          valign = "center";
-        };
-
-        label = {
-          text = "$TIME";
-          font_family = "JetBrains Mono Extrabold";
-          font_size = 400;
-          position = "0, 0";
-          halign = "center";
-          valign = "center";
-        };
       };
     };
   };
