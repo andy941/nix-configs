0d2a1f4020ca23f375ce85add34caf477478b607
Andrea Finocchio <finocchio.andrea94@gmail.com>
2026-01-17 01:44:06 +0000

    Fix suspension

--- hm_modules/hyprland/hypridle.nix
+++ hm_modules/hyprland/hypridle.nix
@@ -3,36 +3,6 @@
 {
   options = { hypridle.enable = lib.mkEnableOption "enables hypridle"; };
 
-  config = lib.mkIf config.hypridle.enable {
-    services.hypridle = {
-      enable = true;
-
-      settings = {
-        general = {
-          lock_cmd = "pidof hyprlock || hyprlock";
-          unlock_cmd = "notify-send 'unlock!'";
-          
-          inhibit_sleep = 1;
-          before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "sleep 2 && hyprctl dispatch dpms on";
-        };
-
-        listener = [
-          {
-            timeout = 900;
-            on-timeout = "loginctl lock-session";
-          }
-          {
-            timeout = 1200;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
-          }
-          # {
-          #   timeout = 1800;
-          #   on-timeout = "systemctl suspend";
-          # }
-        ];
-      };
-    };
-  };
+  config =
+    lib.mkIf config.hypridle.enable { services.hypridle = { enable = true; }; };
 }
