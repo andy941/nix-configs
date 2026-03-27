e86cba51f466d1a0846564e61010864929e4d0ed
Andrea Finocchio <finocchio.andrea94@gmail.com>
2026-03-27 22:09:02 +0000

    Great TMUX defaults

--- hm_modules/tmux.nix
+++ hm_modules/tmux.nix
@@ -13,6 +13,29 @@
   config = lib.mkIf config.tmux.enable {
     programs.tmux = {
       enable = true;
+      keyMode = "vi";
+      shortcut = "a";
+      customPaneNavigationAndResize = true;
+      extraConfig = ''
+        set-option -g status-position top
+      '';
+      plugins = with pkgs; [
+        tmuxPlugins.cpu
+        {
+          plugin = tmuxPlugins.resurrect;
+          extraConfig = ''
+            set -g @resurrect-strategy-nvim 'session'
+            set -g @resurrect-capture-pane-contents 'on'
+          '';
+        }
+        {
+          plugin = tmuxPlugins.continuum;
+          extraConfig = ''
+            set -g @continuum-restore 'on'
+            set -g @continuum-save-interval '60' # minutes
+          '';
+        }
+      ];
     };
   };
 }
